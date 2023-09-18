11d947681d160718da161049a9570218d182d3a6
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-09-19 00:02:30 +0200

    added styles created conttroled function

--- src/CurrencySelect/index.js
+++ src/CurrencySelect/index.js
@@ -2,24 +2,65 @@ import "./style.css";
 import { useState } from "react";
 
 const CurrencySelect = () => {
+    const currencies = [
+        { label: "EUR", value: 0.23, name: "Euro" },
+        { label: "USD", value: 0.25, name: "Dolar Amerykański" },
+        { label: "ARS", value: 66.67, name: "Peso Argentyńskie" },
+    ];
+
     const [selectedOption, setSelectedOption] = useState("EUR");
+    const [amount, setAmount] = useState(0);
+    const [result, setResult] = useState(0);
+
     const currencyChange = (event) => {
-        setSelectedOption(event.target.value)
+        setSelectedOption(event.target.value);
+        convertCurrency(amount, event.target.value);
     };
 
-    const currencies = [
-        { label: "EUR", value: 4.46, name: "Euro" },
-        { label: "USD", value: 4.01, name: "Dolar Amerykański" },
-        { label: "ARS", value: 0.015, name: "Peso Argentyńskie" },
-    ];
+    const convertCurrency = (inputAmount, targetCurrency) => {
+        const selectedCurrency = currencies.find((currency) => currency.label === targetCurrency);
+        if (selectedCurrency) {
+            const convertedResult = inputAmount / selectedCurrency.value;
+            setResult(convertedResult);
+        };
+    };
+
+    const handleAmountChange = (event) => {
+        const newAmount = parseFloat(event.target.value);
+        if (!isNaN(newAmount)) {
+            setAmount(newAmount);
+            convertCurrency(newAmount, selectedOption);
+        };
+    };
+
     return (
-        <select className="selectCurrency" value={selectedOption} onChange={currencyChange}>
-            {currencies.map((currency) => (
-                <option value={currency.label} key={currency.label}>
-                    {currency.name}
-                </option>
-            ))};
-        </select>
+        <>
+            <div className="selectContainer">
+                <select
+                    className="selectCurrency"
+                    value={selectedOption}
+                    onChange={currencyChange}
+                >
+                    {currencies.map((currency) => (
+                        <option value={currency.label} key={currency.label}>
+                            {currency.name}
+                        </option>
+                    ))};
+                </select>
+                <p>
+                    Wpisz ilość w złotówkach
+                </p>
+                <input
+                    className="selectContainer__input"
+                    type="number"
+                    min={0.00}
+                    step={0.01}
+                    value={amount}
+                    onChange={handleAmountChange}
+                />
+            </div>
+            <p className="result">{result} {selectedOption}</p>
+        </>
     );
 };
 

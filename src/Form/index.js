92ee868d900042b246e523218079604e3b2f6088
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-09-18 23:58:49 +0200

    refractor form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,9 +3,7 @@ import Legend from "../Legend";
 import Label from "../Label";
 import Footer from "../Footer";
 import Button from "../Button";
-import Result from "../Result";
 import CurrencySelect from "../CurrencySelect";
-import { useState } from "react";
 
 
 const Form = () => {
@@ -13,37 +11,16 @@ const Form = () => {
     event.preventDefault();
   };
 
-  const [amount, setAmount] = useState("");
+
 
   return (
     <form className="form" onSubmit={onFormSubmit} >
       <fieldset className="fieldset">
         <Legend legend="Kalkulator walutowy" />
-
-        <div className="labels">
-          <Label
-            text="Wprowadź kwotę w zł żeby przewalutować: "
-            body={
-              <input
-                className="label__amount"
-                value={amount}
-                onChange={({ target }) => setAmount(target.value)}
-                name="amount"
-                required
-                placeholder="0 zł"
-                type="number"
-                step="0.01"
-                min="0.01"
-                max="100000"
-              />
-            }
-          />
-          <Label
-            text="Wybierz walutę: "
-            body={<CurrencySelect />}
-          />
-        </div>
-        <Result />
+        <Label
+          className="label"
+          body={<CurrencySelect />}
+        />
         <Button text="Przelicz!" />
         <Footer body="Kursy walut z dnia 23.07.23" />
       </fieldset>
--- src/Result/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const Result = () => (
-    <p className="result">
-        Kwota którą otrzymasz to:
-    </p>
-);
-
-export default Result;

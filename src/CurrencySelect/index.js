0f835cfb3d1900801e8e0a743cdc31a66019e534
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-09-19 09:46:44 +0200

    moved functions to Form component

--- src/CurrencySelect/index.js
+++ src/CurrencySelect/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { useState } from "react";
 
-const CurrencySelect = () => {
+const CurrencySelect = ({ setResult }) => {
     const currencies = [
         { label: "EUR", value: 0.23, name: "Euro" },
         { label: "USD", value: 0.25, name: "Dolar Amerykański" },
@@ -9,29 +9,10 @@ const CurrencySelect = () => {
     ];
 
     const [selectedOption, setSelectedOption] = useState("EUR");
-    const [amount, setAmount] = useState(0);
-    const [result, setResult] = useState(0);
-
     const currencyChange = (event) => {
         setSelectedOption(event.target.value);
-        convertCurrency(amount, event.target.value);
     };
 
-    const convertCurrency = (inputAmount, targetCurrency) => {
-        const selectedCurrency = currencies.find((currency) => currency.label === targetCurrency);
-        if (selectedCurrency) {
-            const convertedResult = inputAmount / selectedCurrency.value;
-            setResult(convertedResult);
-        };
-    };
-
-    const handleAmountChange = (event) => {
-        const newAmount = parseFloat(event.target.value);
-        if (!isNaN(newAmount)) {
-            setAmount(newAmount);
-            convertCurrency(newAmount, selectedOption);
-        };
-    };
 
     return (
         <>
@@ -47,19 +28,7 @@ const CurrencySelect = () => {
                         </option>
                     ))};
                 </select>
-                <p>
-                    Wpisz ilość w złotówkach
-                </p>
-                <input
-                    className="selectContainer__input"
-                    type="number"
-                    min={0.00}
-                    step={0.01}
-                    value={amount}
-                    onChange={handleAmountChange}
-                />
             </div>
-            <p className="result">{result} {selectedOption}</p>
         </>
     );
 };
--- src/Form/index.js
+++ src/Form/index.js
@@ -4,24 +4,70 @@ import Label from "../Label";
 import Footer from "../Footer";
 import Button from "../Button";
 import CurrencySelect from "../CurrencySelect";
-
+import { useState } from "react";
+import Result from "../Result";
 
 const Form = () => {
+  const [result, setResult] = useState(0);
+  const [amount, setAmount] = useState("");
+  const [selectedOption, setSelectedOption] = useState("EUR");
+
+
+  const currencies = [
+    { label: "EUR", value: 0.23, name: "Euro" },
+    { label: "USD", value: 0.25, name: "Dolar Amerykański" },
+    { label: "ARS", value: 66.67, name: "Peso Argentyńskie" },
+  ];
+
   const onFormSubmit = (event) => {
     event.preventDefault();
+    convertCurrency();
+  };
+
+  const handleAmountChange = (newAmount) => {
+    setAmount(newAmount);
+  };
+  const handleCurrencyChange = (newCurrency) => {
+    setSelectedOption(newCurrency);
+  };
+
+  const convertCurrency = (convertedResult) => {
+    const selectedCurrencyValue = currencies.find((currency) => currency.label === selectedOption)?.value;
+    if (selectedCurrencyValue !== undefined) {
+      const convertedResult = (parseFloat(amount) / selectedCurrencyValue).toFixed(2);
+      setResult(convertedResult);
+    } else {
+      setResult("Wybierz poprawną walutę"); // Możesz dodać obsługę błędu, gdy wybrana waluta jest nieznana
+    };
+    return convertedResult;
   };
 
 
 
   return (
-    <form className="form" onSubmit={onFormSubmit} >
+    <form currencies={currencies} className="form" onSubmit={onFormSubmit} >
       <fieldset className="fieldset">
         <Legend legend="Kalkulator walutowy" />
         <Label
           className="label"
-          body={<CurrencySelect />}
+          body={<CurrencySelect onCurrencyChange={handleCurrencyChange} setResult={setResult} />}
         />
-        <Button text="Przelicz!" />
+        <div className="label__container">
+          <input
+            className="label__amount"
+            value={amount}
+            onChange={(event) => handleAmountChange(event.target.value)}
+            name="amount"
+            required
+            placeholder="0 zł"
+            type="number"
+            step="0.01"
+            min="0.01"
+            max="100000"
+          />
+          <Button convertCurrency={convertCurrency} text="Przelicz!" />
+        </div>
+        <Result result={result} />
         <Footer body="Kursy walut z dnia 23.07.23" />
       </fieldset>
     </form>

51de5cdd91cb7b303ef7383d4a0b80e22ebb570a
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-08 13:02:14 +0200

    cosmetic change

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,12 +1,10 @@
 import { ResultContainer, ResultText } from "./styled";
 
-const Result = ({ result, selectedCurrency }) => {
-    return (
-        <ResultContainer>
-            <h2>Po przewalutowaniu otrzymasz:</h2>
-            <ResultText>{result} {selectedCurrency}</ResultText>
-        </ResultContainer>
-    );
-};
+const Result = ({ result, selectedCurrency }) => (
+    <ResultContainer>
+        <h2>Po przewalutowaniu otrzymasz:</h2>
+        <ResultText>{result} {selectedCurrency}</ResultText>
+    </ResultContainer>
+);
 
 export default Result;

392afdf270e41cc42d0da611b5b50a84b00611d8
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-10-10 18:18:40 +0200

    deleted component currency select

--- src/CurrencySelect/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Select = styled.select`
-    width: 160px;
-    margin: 0 auto;
-    text-align: center;
-    background-color: ${({ theme }) => theme.color.Alto};
-    border-radius: 10px;
-`;
